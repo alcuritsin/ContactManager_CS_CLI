59bded12a6afbde7d88360bc10be30bf68808abf
alcuritsin <al.curitsin@gmail.com>
2021-08-09 23:53:52 +0500

    Класс Contact в процесе описания. Нужно написать и протестировать сериализацию...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
 
 namespace ContactManager
 {
     class Program
     {
+        public class Contact
+        {
+            public string ContactID { get; set; }           //  id
+            public string Surname { get; set; }             //  Фамилия
+            public string Name { get; set; }                //  Имя
+            public string Patronymic { get; set; }          //  Отчество
+            //public DateOfBirth DateOfBirth { get; set; }    //  Дата рождения
+            //public PhoneNumbers PhoneNumbers { get; set; }  //  Телефонные номера
+            //public Email Email { get; set; }                //  Клектронный адрес
+            public string City { get; set; }
+            Contact() { }
+            ~Contact() { }
+
+            public Contact(
+                string _contactID,
+                string _surname,
+                string _name,
+                string _patronymic,
+                string _city)
+            {
+                ContactID = _contactID;
+                Surname = _surname;
+                Name = _name;
+                Patronymic = _patronymic;
+                City = _city;
+            }
+
+            public void info()
+            {
+                WriteLine($"     id: {ContactID}");
+                WriteLine($"contact: {Surname} {Name} {Patronymic}");
+                WriteLine($"   city: {City}");
+            }
+        }
+        
         static void Main()
         {
-            
+            List<Contact> contacts = new List<Contact>(3);
+
         }
     }
 }
